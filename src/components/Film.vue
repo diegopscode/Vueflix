a94c62eb8ffecdb458aeb5cde1af06c28236deec
Diego <diego_pancera@hotmail.com>
2017-08-02 20:44:04 -0300

    Adicionado componente de filme

---
+++ src/components/Film.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="film">
+    <div class="film-image">
+        <img :src="data.poster" alt="Capa do Filme">
+    </div>
+    <div class="film-content">
+        <div class="flex-duo">
+            <h2 class="title">{{ data.show_title }}</h2>
+            <div class="rate"><b>Netflix Rating:</b> {{ data.rating }}</div>
+        </div>
+
+        <div class="summary"><b>Summary:</b> {{ data.summary }}</div>
+        
+        <div class="director"> <b>Director:</b> {{ data.director }} </div>
+
+        <div class="cast"><b>Cast:</b> {{ data.show_cast }}</div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'film',
+    props: {
+        data: {
+            type: Object,
+            required: true
+        },
+    }
+  }
+</script>
--- src/scenes/Home.vue
+++ src/scenes/Home.vue
@@ -2,19 +2,34 @@
   <section class="content">
     <div class="container">
 
-      <h2>{{ title }}</h2>
+      <Film :data="film"></Film>
       
     </div>
   </section>
 </template>
 
 <script>
+  import Film from "../components/Film"
+
   export default {
     name: 'home',
+    components: {
+      Film
+    },
     data() {
       return {
-        title: "Conteúdo"
+        title: 'Attack on Titan',
+        search: '',
+        film: {}
       }
+    },
+    methods: {
+      searchFilm(title) {
+        this.$store.dispatch('SEARCH_FILM', title).then(data => { this.film = data; console.log(data) })
+      }
+    },
+    mounted() {
+      this.searchFilm(this.title)
     }
   }
 </script>
