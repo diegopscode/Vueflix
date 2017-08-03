6755307810bc9e9511ae562b1b6c7823e6415678
Diego <diego_pancera@hotmail.com>
2017-08-03 13:34:00 -0300

    Componente de pesquisa adicionado

---
+++ src/components/Search.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="search">
+    <div class="search-content">
+            
+        <form class="form-search" @click.prevent="searchFilm()">
+            <div class="form-group search-input">
+                <input type="text" class="form-input" placeholder="Title">
+            </div>
+
+            <div class="form-group search-input">
+                <input type="text" class="form-input"  placeholder="Director">
+            </div>
+
+            <div class="form-group search-input">
+                <input type="text" class="form-input"  placeholder="Actor">
+            </div>
+
+            <div class="form-button">
+                <button class="btn btn-submit">Search</button>
+            </div>
+        </form>
+
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'search',
+    data () {
+        return {
+            search: {}
+        }
+    },
+    methods: {
+        searchFilm() {
+            console.log(this.search);
+        }
+    }
+  }
+</script>
